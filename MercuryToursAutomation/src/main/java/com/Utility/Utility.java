099f565d64edd31767d75fc3a8bde2680e517bc4
rsrivastava-rxlogix <44287179+rsrivastava-rxlogix@users.noreply.github.com>
2018-12-28 19:23:50 +0530

    properties file created
    properties file created

---
+++ MercuryToursAutomation/src/main/java/com/Utility/Utility.java
@@ -0,0 +1,46 @@
+package com.Utility;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Utility {
+
+		  //public static void main(String[] args) {
+
+			  public void FetchValue(String strValue)
+			  {
+				  Properties prop = new Properties();
+				    InputStream input = null;
+
+				    
+				    try {
+				        input = new FileInputStream("C:\\Users\\Lenovo\\Desktop\\MercuryTours\\MercuryToursAutomation\\src\\main\\java\\com\\resources\\config.properties");
+
+				        // load a properties file
+				        
+							prop.load(input);
+							System.out.println(prop.getProperty(strValue));
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (Exception ex) {
+							ex.printStackTrace();
+						}  finally {
+						    if (input != null) {
+						        try {
+						            input.close();
+						        } catch (IOException e) {
+						            e.printStackTrace();
+						        }
+						    }
+						}
+ 
+			  }
+			    
+			        
+			        
+	}
+
+//}
---
+++ MercuryToursAutomation/src/test/java/Rough.java
@@ -0,0 +1,12 @@
+import com.Utility.Utility;
+
+public class Rough {
+	public static void main(String[] sonu)
+	{
+		Utility obj=new Utility();
+				obj.FetchValue("url");
+		//Utility.FetchValue("strUserName");
+		
+	}
+
+}
